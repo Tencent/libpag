cbb7f43e05ba802f53d32b176e13d8e4e6b85bff
pengweilv <pengwei.lv@gmail.com>
2022-01-18 21:19:49 +0800

    Release the PAGSurface before the GLSurfaceView destroys context.

--- android/demo/src/main/java/io/pag/demo/TextureDemoActivity.java
+++ android/demo/src/main/java/io/pag/demo/TextureDemoActivity.java
@@ -3,10 +3,17 @@ package io.pag.demo;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.util.Log;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
 
 import io.pag.demo.openGL.GLRender;
 
 public class TextureDemoActivity extends Activity {
+    private final static String TAG = TextureDemoActivity.class.getSimpleName();
 
     private GLSurfaceView glSurfaceView;
 
@@ -14,12 +21,34 @@ public class TextureDemoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_texture_demo);
-        glSurfaceView = (GLSurfaceView) findViewById(R.id.surfaceView);
-        GLRender glRender = new GLRender(this.getBaseContext());
+        glSurfaceView = findViewById(R.id.surfaceView);
+        final GLRender glRender = new GLRender(this.getBaseContext());
         glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setEGLWindowSurfaceFactory(new GLSurfaceView.EGLWindowSurfaceFactory() {
+            public EGLSurface createWindowSurface(EGL10 egl, EGLDisplay display,
+                                                  EGLConfig config, Object nativeWindow) {
+                EGLSurface result = null;
+                try {
+                    result = egl.eglCreateWindowSurface(display, config, nativeWindow, null);
+                } catch (IllegalArgumentException e) {
+                    // This exception indicates that the surface flinger surface
+                    // is not valid. This can happen if the surface flinger surface has
+                    // been torn down, but the application has not yet been
+                    // notified via SurfaceHolder.Callback.surfaceDestroyed.
+                    // In theory the application should be notified first,
+                    // but in practice sometimes it is not. See b/4588890
+                    Log.e(TAG, "eglCreateWindowSurface", e);
+                }
+                return result;
+            }
+
+            public void destroySurface(EGL10 egl, EGLDisplay display, EGLSurface surface) {
+                glRender.onDestroy();
+                egl.eglDestroySurface(display, surface);
+            }
+        });
         glSurfaceView.setRenderer(glRender);
         glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-
     }
 
     @Override
