a58f3c6e71cd685ba1f7cd2af9829d2b2b44bd46
zenoslin@outlook.com <zenoslin@outlook.com>
2022-01-19 10:42:15 +0800

    ADD Vue2 demo.

---
+++ web/demo/vue/vue2/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <canvas class="canvas" id="pag"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    window.libpag.PAGInit().then((PAG) => {
+      const url = 'https://pag.io/file/like.pag';
+      fetch(url)
+        .then((response) => response.blob())
+        .then(async (blob) => {
+          const file = new window.File([blob], url.replace(/(.*\/)*([^.]+)/i, '$2'));
+          const pagFile = await PAG.PAGFile.load(file);
+          document.getElementById('pag').width = await pagFile.width();
+          document.getElementById('pag').height = await pagFile.height();
+          const pagView = await PAG.PAGView.init(pagFile, '#pag');
+          pagView.setRepeatCount(0);
+          await pagView.play();
+        });
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
