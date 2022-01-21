5110a6fa8b1da6d4c596d861a7393924d23a364f
pengweilv <pengwei.lv@gmail.com>
2022-01-21 21:02:56 +0800

    Colors need to be premultiplied.

--- tgfx/src/gpu/opengl/GLConstColorProcessor.cpp
+++ tgfx/src/gpu/opengl/GLConstColorProcessor.cpp
@@ -26,6 +26,10 @@ void GLConstColorProcessor::emitCode(EmitArgs& args) {
   colorUniform = args.uniformHandler->addUniform(ShaderFlags::Fragment, ShaderVar::Type::Float4,
                                                  "Color", &colorName);
   fragBuilder->codeAppendf("%s = %s;", args.outputColor.c_str(), colorName.c_str());
+  const auto* fp = static_cast<const ConstColorProcessor*>(args.fragmentProcessor);
+  if (fp->color.a != 1.0f) {
+    fragBuilder->codeAppendf("%s.rgb *= %s.a;", args.outputColor.c_str(), args.outputColor.c_str());
+  }
 }
 
 void GLConstColorProcessor::onSetData(const ProgramDataManager& programDataManager,
