7b1c3e648166dc4124b05a51a43ce92b4ca3fd01
sovea <harleyhhh423@gmail.com>
2022-01-26 14:17:19 +0800

    Update vue demo.

---
+++ web/demo/vue/vue2/config/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  dev: {
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    host: 'localhost',
+  },
+
+  build: {
+    // Template for index.html
+    index: path.resolve(__dirname, '../dist/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
+  }
+}
