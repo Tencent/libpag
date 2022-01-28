944450eed730ca9fc53bbecf1c71cbc6836dd4c7
kevingpqi123 <kevingpqi@gmail.com>
2022-01-28 11:28:09 +0800

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -9,6 +9,8 @@ echo "shell log - autotest start begin "
 
 echo `pwd`
 
+COMPLIE_RESULT=true
+
 WORKSPACE=$(pwd)
 
 cd $WORKSPACE
@@ -18,13 +20,13 @@ make_dir build
 
 cd build
 
-cmake -DcppFlags="-fprofile-arcs -ftest-coverage -g -O0" -DSMOKE_TEST=ON ../
+cmake -DcppFlags="-fprofile-arcs -ftest-coverage -g -O0" -DSMOKE_TEST=ON -DCMAKE_BUILD_TYPE=Debug ../
 if test $? -eq 0
 then
 echo "~~~~~~~~~~~~~~~~~~~CMakeLists OK~~~~~~~~~~~~~~~~~~"
 else
 echo "~~~~~~~~~~~~~~~~~~~CMakeLists error~~~~~~~~~~~~~~~~~~"
-exit -1
+exit
 fi
 
 cmake --build . --target PAGFullTest -- -j 12
@@ -33,10 +35,10 @@ then
 echo "~~~~~~~~~~~~~~~~~~~PAGFullTest make successed~~~~~~~~~~~~~~~~~~"
 else
 echo "~~~~~~~~~~~~~~~~~~~PAGFullTest make error~~~~~~~~~~~~~~~~~~"
-exit -1
+exit
 fi
 
-./PAGFullTest --gtest_output=json 
+./PAGFullTest --gtest_output=json:PAGFullTest.json
 
 if test $? -eq 0
 
@@ -44,9 +46,33 @@ then
 echo "~~~~~~~~~~~~~~~~~~~PAGFullTest successed~~~~~~~~~~~~~~~~~~"
 else
 echo "~~~~~~~~~~~~~~~~~~~PAGFullTest Failed~~~~~~~~~~~~~~~~~~"
-cp -a $WORKSPACE/test/out/baseline/**/*.lzma2 $WORKSPACE/result/
-cp -a $WORKSPACE/test/out/compare/*.webp $WORKSPACE/result/
-exit -1
+COMPLIE_RESULT=false
 fi
 
-cp -a $WORKSPACE/build/test_detail.json $WORKSPACE/result/
+cmake --build . --target PAGSmokeTest -- -j 12
+if test $? -eq 0
+then
+echo "~~~~~~~~~~~~~~~~~~~PAGSmokeTest make successed~~~~~~~~~~~~~~~~~~"
+else
+echo "~~~~~~~~~~~~~~~~~~~PAGSmokeTest make error~~~~~~~~~~~~~~~~~~"
+exit
+fi
+
+./PAGSmokeTest --gtest_output=json:PAGSmokeTest.json
+
+if test $? -eq 0
+
+then
+echo "~~~~~~~~~~~~~~~~~~~PAGSmokeTest successed~~~~~~~~~~~~~~~~~~"
+else
+echo "~~~~~~~~~~~~~~~~~~~PAGSmokeTest Failed~~~~~~~~~~~~~~~~~~"
+COMPLIE_RESULT=false
+fi
+
+if [ "$COMPLIE_RESULT" == false ]
+then
+ cp -a $WORKSPACE/test/out/baseline/**/*.lzma2 $WORKSPACE/result/
+ cp -a $WORKSPACE/test/out/compare/*.webp $WORKSPACE/result/
+fi
+
+cp -a $WORKSPACE/build/*.json $WORKSPACE/result/
