4fb51257cd868d53061d0caa0d351d8d4f24aad3
kevingpqi <kevingpqi@tencent.com>
2022-02-05 14:33:27 +0800

    update

--- codeformat.sh
+++ codeformat.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 echo "----begin coding format----"
-find ./include -iname "*.h" -print0 | xargs clang-format -i
-find ./src -iname "*.h" -0 -iname "*.cpp" -path "./src/platform/ios" -path "./src/platform/mac" -print0 | xargs clang-format -i
-find ./test -iname "*.cpp" -print0 | xargs clang-format -i
-find ./tgfx -iname "*.h" -o -iname "*.cpp" -print0 | xargs clang-format -i
-echo $result
+find include/ -iname '*.h' -print0 | xargs clang-format -i
+find tgfx/include -iname '*.h' -print0 | xargs clang-format -i
+# shellcheck disable=SC2038
+find tgfx/src -iname "*.h" -print -o -iname "*.cpp" -print  | xargs clang-format -i
+# shellcheck disable=SC2038
+find src \( -path src/platform/ios -o -path src/platform/mac -o -path src/platform/cocoa \) -prune -o -name "*.cpp" -print  -o -name "*.h" -print | xargs clang-format -i
+# shellcheck disable=SC2038
+find test \( -path test/framework/lzma \) -prune -o -name "*.cpp" -print  -o -name "*.h" -print | xargs clang-format -i
 
+git diff
+result=`git diff`
 if [[ $result =~ "diff" ]]
 then
     echo "----Failed to pass coding specification----"
