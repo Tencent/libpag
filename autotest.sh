d41d468b388951c67fc15e66de4febaa90032eb0
kevingpqi <kevingpqi@tencent.com>
2022-02-07 00:56:37 +0800

    update

--- autotest.sh
+++ autotest.sh
@@ -6,6 +6,20 @@ function make_dir() {
 }
 
 echo "shell log - autotest start begin "
+if [[ `uname` == 'Darwin' ]]; then
+  MAC_REQUIRED_TOOLS="gcovr"
+  for TOOL in ${MAC_REQUIRED_TOOLS[@]}; do
+  if [ ! $(which $TOOL) ]; then
+    if [ ! $(which brew) ]; then
+      echo "Homebrew not found. Trying to install..."
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" ||
+        exit 1
+    fi
+    echo "$TOOL not found. Trying to install..."
+    brew install $TOOL || exit 1
+  fi
+  done
+fi
 
 echo `pwd`
 
