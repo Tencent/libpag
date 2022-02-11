69dc484855326e0fe86c7df9fe50466753cdb4ae
kevingpqi123 <kevingpqi@gmail.com>
2022-02-11 15:57:32 +0800

    Update codeformat.sh

--- codeformat.sh
+++ codeformat.sh
@@ -9,15 +9,5 @@ find tgfx/src -iname "*.h" -print -o -iname "*.cpp" -print  | xargs clang-format
 find src \( -path src/platform/ios -o -path src/platform/mac -o -path src/platform/cocoa \) -prune -o -name "*.cpp" -print  -o -name "*.h" -print | xargs clang-format -i
 # shellcheck disable=SC2038
 find test \( -path test/framework/lzma \) -prune -o -name "*.cpp" -print  -o -name "*.h" -print | xargs clang-format -i
-
-git diff
-result=`git diff`
-if [[ $result =~ "diff" ]]
-then
-    echo "----Failed to pass coding specification----"
-    exit 1
-else
-    echo "----Pass coding specification----"
-fi
 echo "----end coding format----"
 
