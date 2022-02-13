3dde04bd76e757d9f0e980e50f9825c62e457c4f
Dom <dom@idom.me>
2022-02-13 13:34:58 +0800

    Add YUVInfo.h

--- tgfx/include/gpu/YUVTexture.h
+++ tgfx/include/gpu/YUVTexture.h
@@ -19,66 +19,9 @@
 #pragma once
 
 #include "Texture.h"
+#include "core/YUVInfo.h"
 
 namespace pag {
-/**
- * Defines pixel formats for YUVTexture.
- */
-enum class YUVPixelFormat {
-  /**
-   * uninitialized.
-   */
-  Unknown,
-  /**
-   * 8 bit Y plane followed by 8 bit 2x2 subsampled U and V planes.
-   */
-  I420,
-  /**
-   * 8-bit Y plane followed by an interleaved U/V plane with 2x2 subsampling.
-   */
-  NV12
-};
-
-/**
- *  Describes color space of YUV pixels. The color mapping from YUV to RGB varies depending on the
- *  source.
- */
-enum class YUVColorSpace {
-  /**
-   * uninitialized.
-   */
-  Unknown,
-  /**
-   * Describes SDTV range.
-   */
-  Rec601,
-  /**
-   * Describes HDTV range.
-   */
-  Rec709,
-  /**
-   * Describes UHDTV range.
-   */
-  Rec2020
-};
-
-/**
- *  Describes color range of YUV pixels.
- */
-enum class YUVColorRange {
-  /**
-   * uninitialized.
-   */
-  Unknown,
-  /**
-   * the normal 219*2^(n-8) "MPEG" YUV ranges
-   */
-  MPEG,
-  /**
-   * the normal 2^n-1 "JPEG" YUV ranges
-   */
-  JPEG
-};
 
 class YUVTexture : public Texture {
  public:
