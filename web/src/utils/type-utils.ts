6edf552816205b3e9e618064aed145adb5e371c1
zenoslin <zenoslin@outlook.com>
2022-03-28 19:57:16 +0800

    Fix vector function async error on Web env.

--- web/src/utils/type-utils.ts
+++ web/src/utils/type-utils.ts
@@ -1,21 +1,37 @@
 import { Vector } from '../types';
 
+const rewindData = (module: any, fn: (...args: any[]) => any, scope: any, ...args: any[]) => {
+  if (module.Asyncify.currData !== null) {
+    const currData = module.Asyncify.currData;
+    module.Asyncify.currData = null;
+    const ret = fn.call(scope, ...args);
+    module.Asyncify.currData = currData;
+    ret();
+  } else {
+    return fn.call(scope, ...args);
+  }
+};
+
 export const proxyVector = <T extends { wasmIns: any }>(
   vector: Vector<any>,
-  constructor: new (wasmIns: any) => T,
+  constructor: { module: any; new (wasmIns: any): T },
 ): Vector<T> => {
   const proxy = new Proxy(vector, {
     get(target, property, receiver) {
       switch (property) {
         case 'get':
           return (index: number) => {
-            const wasmIns = target.get(index);
+            const wasmIns = rewindData(constructor.module, target.get, target, index);
             return wasmIns ? new constructor(wasmIns) : wasmIns;
           };
         case 'push_back':
           return (value: T) => {
-            target.push_back(value.wasmIns);
+            rewindData(constructor.module, target.push_back, target, value.wasmIns);
             return undefined;
+          };
+        case 'size':
+          return () => {
+            return rewindData(constructor.module, target.size, target);
           };
         default:
           return Reflect.get(target, property, receiver);
