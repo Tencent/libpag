8493d418310c8b4d856f53e78a557ae8ce7f68a4
pengweilv <pengwei.lv@gmail.com>
2022-04-26 18:46:13 +0800

    Use surface to apply clip.

--- tgfx/src/gpu/opengl/GLCanvas.cpp
+++ tgfx/src/gpu/opengl/GLCanvas.cpp
@@ -44,24 +44,24 @@ void GLCanvas::drawTexture(const Texture* texture, const Texture* mask, bool inv
 }
 
 Texture* GLCanvas::getClipTexture() {
-  if (_clipMask == nullptr) {
-    _clipMask = Mask::Make(surface->width(), surface->height());
+  if (_clipSurface == nullptr) {
+    _clipSurface = Surface::Make(getContext(), surface->width(), surface->height(), true);
+    if (_clipSurface == nullptr) {
+      _clipSurface = Surface::Make(getContext(), surface->width(), surface->height());
+    }
   }
-  if (_clipMask == nullptr) {
+  if (_clipSurface == nullptr) {
     return nullptr;
   }
   if (clipID != state->clipID) {
-    _clipMask = Mask::Make(surface->width(), surface->height());
-    _clipMask->fillPath(state->clip);
+    auto clipCanvas = _clipSurface->getCanvas();
+    clipCanvas->clear();
+    Paint paint = {};
+    paint.setColor(Color::Black());
+    clipCanvas->drawPath(state->clip, paint);
     clipID = state->clipID;
-    // recycle the clip texture.
-    _clipTexture = nullptr;
-    _clipTexture = _clipMask->makeTexture(getContext());
   }
-  if (_clipTexture == nullptr) {
-    _clipTexture = _clipMask->makeTexture(getContext());
-  }
-  return _clipTexture.get();
+  return _clipSurface->getTexture().get();
 }
 
 static constexpr float BOUNDS_TO_LERANCE = 1e-3f;
