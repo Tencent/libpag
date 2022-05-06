e1d870f9f64e2c9d02e54703613ffa24c36d3828
lvpengwei <pengwei.lv@gmail.com>
2022-05-06 16:05:24 +0800

    Fix wild pointer for reading png. (#315)

--- tgfx/src/core/images/png/PngImage.cpp
+++ tgfx/src/core/images/png/PngImage.cpp
@@ -25,6 +25,7 @@ namespace tgfx {
 std::shared_ptr<Image> PngImage::MakeFrom(const std::string& filePath) {
   return MakeFromData(filePath, nullptr);
 }
+
 std::shared_ptr<Image> PngImage::MakeFrom(std::shared_ptr<Data> imageBytes) {
   return MakeFromData("", std::move(imageBytes));
 }
@@ -40,7 +41,7 @@ typedef struct {
   const unsigned char* cursor;
 } PngReader;
 
-static void png_reader_read_data(png_structp png_ptr, png_bytep data, png_size_t length) {
+static void PngReaderReadData(png_structp png_ptr, png_bytep data, png_size_t length) {
   auto reader = static_cast<PngReader*>(png_get_io_ptr(png_ptr));
   auto avail = static_cast<png_size_t>(reader->end - reader->cursor);
   if (avail > length) {
@@ -50,59 +51,76 @@ static void png_reader_read_data(png_structp png_ptr, png_bytep data, png_size_t
   reader->cursor += avail;
 }
 
-std::shared_ptr<Image> PngImage::MakeFromData(const std::string& filePath,
-                                              std::shared_ptr<Data> byteData) {
-  FILE* infile = nullptr;
-  if (byteData == nullptr && (infile = fopen(filePath.c_str(), "rb")) == nullptr) {
-    return nullptr;
-  }
-  png_structp p = nullptr;
-  png_infop pi = nullptr;
-  png_uint_32 w = 0, h = 0;
-  p = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-  do {
-    if (p == nullptr) {
-      break;
+class ReadInfo {
+ public:
+  static std::shared_ptr<ReadInfo> Make(const std::string& filePath,
+                                        const std::shared_ptr<Data>& fileData) {
+    auto readInfo = std::make_shared<ReadInfo>();
+    if (!filePath.empty()) {
+      readInfo->infile = fopen(filePath.c_str(), "rb");
     }
-    pi = png_create_info_struct(p);
-    if (pi == nullptr) {
-      break;
-    }
-    if (setjmp(png_jmpbuf(p))) {
-      png_destroy_read_struct(&p, &pi, nullptr);
-      if (infile) {
-        fclose(infile);
-      }
+    if (fileData == nullptr && readInfo->infile == nullptr) {
       return nullptr;
     }
-
-    if (infile) {
-      unsigned char header[8];
-      png_init_io(p, infile);
-      if (fread(header, 8, 1, infile) != 1) break;
-      if (png_sig_cmp(header, 0, 8)) break;
-    } else {
-      PngReader reader;
-      reader.base = byteData->bytes();
-      reader.end = byteData->bytes() + byteData->size();
-      reader.cursor = byteData->bytes();
-      if (byteData->size() < 8 || png_sig_cmp((unsigned char*)byteData->data(), 0, 8)) {
-        break;
-      }
-      reader.cursor += 8;
-      png_set_read_fn(p, &reader, png_reader_read_data);
+    readInfo->p = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    if (readInfo->p == nullptr) {
+      return nullptr;
     }
+    readInfo->pi = png_create_info_struct(readInfo->p);
+    if (readInfo->pi == nullptr) {
+      return nullptr;
+    }
+#ifdef PNG_SET_OPTION_SUPPORTED
+    png_set_option(readInfo->p, PNG_MAXIMUM_INFLATE_WINDOW, PNG_OPTION_ON);
+#endif
+    if (setjmp(png_jmpbuf(readInfo->p))) {
+      return nullptr;
+    }
+    if (readInfo->infile) {
+      png_init_io(readInfo->p, readInfo->infile);
+    } else {
+      readInfo->reader = new PngReader();
+      readInfo->reader->base = fileData->bytes();
+      readInfo->reader->end = fileData->bytes() + fileData->size();
+      readInfo->reader->cursor = fileData->bytes();
+      png_set_read_fn(readInfo->p, readInfo->reader, PngReaderReadData);
+    }
+    png_read_info(readInfo->p, readInfo->pi);
+    return readInfo;
+  }
 
-    png_set_sig_bytes(p, 8);
-    png_read_info(p, pi);
-    png_get_IHDR(p, pi, &w, &h, nullptr, nullptr, nullptr, nullptr, nullptr);
-  } while (false);
-  if (p) {
-    png_destroy_read_struct(&p, &pi, nullptr);
+  ~ReadInfo() {
+    if (p) {
+      png_destroy_read_struct(&p, &pi, nullptr);
+    }
+    if (data) {
+      free(data);
+    }
+    if (rowPtrs) {
+      free(rowPtrs);
+    }
+    if (infile) {
+      fclose(infile);
+    }
+    delete reader;
   }
-  if (infile) {
-    fclose(infile);
+
+  png_structp p = nullptr;
+  png_infop pi = nullptr;
+  FILE* infile = nullptr;
+  PngReader* reader = nullptr;
+  unsigned char** rowPtrs = nullptr;
+  unsigned char* data = nullptr;
+};
+
+std::shared_ptr<Image> PngImage::MakeFromData(const std::string& filePath,
+                                              std::shared_ptr<Data> byteData) {
+  auto readInfo = ReadInfo::Make(filePath, byteData);
+  if (readInfo == nullptr) {
+    return nullptr;
   }
+  png_uint_32 w = 0, h = 0;
+  png_get_IHDR(readInfo->p, readInfo->pi, &w, &h, nullptr, nullptr, nullptr, nullptr, nullptr);
   if (w == 0 || h == 0) {
     return nullptr;
   }
@@ -110,7 +128,7 @@ std::shared_ptr<Image> PngImage::MakeFromData(const std::string& filePath,
                                              Orientation::TopLeft, filePath, std::move(byteData)));
 }
 
-static void updateReadInfo(png_structp p, png_infop pi) {
+static void UpdateReadInfo(png_structp p, png_infop pi) {
   int originalColorType = png_get_color_type(p, pi);
   int bitDepth = png_get_bit_depth(p, pi);
   if (bitDepth == 16) {
@@ -135,113 +153,37 @@ static void updateReadInfo(png_structp p, png_infop pi) {
   png_read_update_info(p, pi);
 }
 
-struct ReadInfo {
-  png_structp p;
-  png_infop pi;
-  FILE* infile;
-  unsigned char** rowPtrs;
-  unsigned char* data;
-};
-
-static void freeReadInfo(ReadInfo* readInfo) {
-  if (readInfo->p) {
-    png_destroy_read_struct(&readInfo->p, &readInfo->pi, nullptr);
-  }
-  if (readInfo->data) {
-    free(readInfo->data);
-  }
-  if (readInfo->rowPtrs) {
-    free(readInfo->rowPtrs);
-  }
-  if (readInfo->infile) {
-    fclose(readInfo->infile);
-  }
-}
-
-static bool prepareReader(ReadInfo* readInfo, const Data* imageBytes) {
-#ifdef PNG_SET_OPTION_SUPPORTED
-  png_set_option(readInfo->p, PNG_MAXIMUM_INFLATE_WINDOW, PNG_OPTION_ON);
-#endif
-  if (setjmp(png_jmpbuf(readInfo->p))) {
-    return false;
-  }
-  if (readInfo->infile) {
-    unsigned char header[8];
-    png_init_io(readInfo->p, readInfo->infile);
-    if (fread(header, 8, 1, readInfo->infile) != 1) {
-      return false;
-    }
-    if (png_sig_cmp(header, 0, 8)) {
-      return false;
-    }
-  } else {
-    PngReader reader;
-    reader.base = imageBytes->bytes();
-    reader.end = imageBytes->bytes() + imageBytes->size();
-    reader.cursor = imageBytes->bytes();
-    if (imageBytes->size() < 8 ||
-        png_sig_cmp(static_cast<png_const_bytep>(imageBytes->data()), 0, 8)) {
-      return false;
-    }
-    reader.cursor += 8;
-    png_set_read_fn(readInfo->p, &reader, png_reader_read_data);
-  }
-
-  png_set_sig_bytes(readInfo->p, 8);
-  png_read_info(readInfo->p, readInfo->pi);
-  return true;
-}
-
 bool PngImage::readPixels(const ImageInfo& dstInfo, void* dstPixels) const {
-  FILE* infile = nullptr;
-  if (fileData == nullptr && (infile = fopen(filePath.c_str(), "rb")) == nullptr) {
+  auto readInfo = ReadInfo::Make(filePath, fileData);
+  if (readInfo == nullptr) {
     return false;
   }
-  png_structp p = nullptr;
-  p = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-  if (p == nullptr) {
-    return false;
-  }
-  png_infop pi = png_create_info_struct(p);
-  if (pi == nullptr) {
-    png_destroy_read_struct(&p, nullptr, nullptr);
-    return false;
-  }
-  ReadInfo readInfo = {p, pi, infile, nullptr, nullptr};
-  bool decodeSuccess = readPixelsInternal(dstInfo, dstPixels, readInfo);
-  freeReadInfo(&readInfo);
-  return decodeSuccess;
-}
-
-bool PngImage::readPixelsInternal(const ImageInfo& dstInfo, void* dstPixels,
-                                  ReadInfo& readInfo) const {
-  prepareReader(&readInfo, fileData.get());
-  int w = static_cast<int>(png_get_image_width(readInfo.p, readInfo.pi));
-  int h = static_cast<int>(png_get_image_height(readInfo.p, readInfo.pi));
+  int w = width();
+  int h = height();
   if (h == 0 || w == 0) {
     return false;
   }
-  updateReadInfo(readInfo.p, readInfo.pi);
-  readInfo.rowPtrs = (unsigned char**)malloc(sizeof(unsigned char*) * h);
+  UpdateReadInfo(readInfo->p, readInfo->pi);
+  readInfo->rowPtrs = (unsigned char**)malloc(sizeof(unsigned char*) * h);
   if (dstInfo.colorType() != ColorType::RGBA_8888 ||
       dstInfo.alphaType() != AlphaType::Unpremultiplied) {
-    readInfo.data = (unsigned char*)malloc((w * 4) * h);
-    for (int i = 0; i < height(); i++) {
-      readInfo.rowPtrs[i] = readInfo.data + ((w * 4) * i);
+    readInfo->data = (unsigned char*)malloc((w * 4) * h);
+    for (int i = 0; i < h; i++) {
+      readInfo->rowPtrs[i] = readInfo->data + ((w * 4) * i);
     }
-    png_read_image(readInfo.p, readInfo.rowPtrs);
-    if ((readInfo.data == nullptr) || (readInfo.rowPtrs == nullptr)) {
+    png_read_image(readInfo->p, readInfo->rowPtrs);
+    if (readInfo->data == nullptr || readInfo->rowPtrs == nullptr) {
       return false;
     }
     ImageInfo info = ImageInfo::Make(w, h, ColorType::RGBA_8888, AlphaType::Unpremultiplied);
-    Bitmap bitmap(info, readInfo.data);
+    Bitmap bitmap(info, readInfo->data);
     return bitmap.readPixels(dstInfo, dstPixels);
   } else {
-    if (readInfo.rowPtrs == nullptr) return false;
-    for (int i = 0; i < height(); i++) {
-      readInfo.rowPtrs[i] = static_cast<unsigned char*>(dstPixels) + ((w * 4) * i);
+    if (readInfo->rowPtrs == nullptr) return false;
+    for (int i = 0; i < h; i++) {
+      readInfo->rowPtrs[i] = static_cast<unsigned char*>(dstPixels) + ((w * 4) * i);
     }
-    png_read_image(readInfo.p, readInfo.rowPtrs);
+    png_read_image(readInfo->p, readInfo->rowPtrs);
     return true;
   }
 }
