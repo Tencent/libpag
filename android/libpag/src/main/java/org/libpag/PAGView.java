21a13bca98ecf668d2aa274e5e771c2c88f50c7a
kevingpqi123 <kevingpqi@gmail.com>
2022-06-30 21:00:29 +0800

    Keep the same precision of encoding and decoding, remove the listener of ValueAnimator later. (#393)
    Co-authored-by: kevingpqi <kevingpqi@tencent.com>

--- android/libpag/src/main/java/org/libpag/PAGView.java
+++ android/libpag/src/main/java/org/libpag/PAGView.java
@@ -405,7 +405,6 @@ public class PAGView extends TextureView implements TextureView.SurfaceTextureLi
         if (pagSurface != null) {
             pagSurface.freeCache();
         }
-        animator.removeUpdateListener(mAnimatorUpdateListener);
 
         boolean surfaceDestroy = true;
         if (g_PAGViewHandler != null && surface != null) {
@@ -852,5 +851,11 @@ public class PAGView extends TextureView implements TextureView.SurfaceTextureLi
         }
         _isAnimatorPreRunning = null;
         doPlay();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        animator.removeUpdateListener(mAnimatorUpdateListener);
     }
 }
