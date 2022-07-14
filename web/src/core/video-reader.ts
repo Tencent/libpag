fd9694231bc62544060a95082c50ba94dbd3b153
zenoslin <zenoslin@outlook.com>
2022-07-14 14:55:59 +0800

    Append video element in DOM tree.

--- web/src/core/video-reader.ts
+++ web/src/core/video-reader.ts
@@ -38,6 +38,8 @@ const playVideoElement = async (videoElement: HTMLVideoElement) => {
   }
 };
 
+let videoReaderBox: HTMLDivElement | null = null;
+
 export class VideoReader {
   public static isIOS = () => {
     return IPHONE;
@@ -48,18 +50,25 @@ export class VideoReader {
   private lastFlush: number;
   private hadPlay = false;
   private staticTimeRanges: StaticTimeRanges;
+  private objectURL: string;
 
   public constructor(mp4Data: Uint8Array, frameRate: number, staticTimeRanges: TimeRange[]) {
     this.videoEl = document.createElement('video');
-    this.videoEl.style.display = 'none';
+    this.videoEl.style.visibility = 'hidden';
     this.videoEl.muted = true;
     this.videoEl.playsInline = true;
+    if (!videoReaderBox) {
+      videoReaderBox = document.createElement('div');
+      videoReaderBox.style.visibility = 'hidden';
+      document.body.appendChild(videoReaderBox);
+    }
+    videoReaderBox.appendChild(this.videoEl);
     this.videoEl.load();
     addListener(this.videoEl, 'timeupdate', this.onTimeupdate.bind(this));
     this.frameRate = frameRate;
     this.lastFlush = -1;
-    const blob = new Blob([mp4Data], { type: 'video/mp4' });
-    this.videoEl.src = URL.createObjectURL(blob);
+    this.objectURL = URL.createObjectURL(new Blob([mp4Data], { type: 'video/mp4' }));
+    this.videoEl.src = this.objectURL;
     this.staticTimeRanges = new StaticTimeRanges(staticTimeRanges);
   }
 
@@ -122,10 +131,13 @@ export class VideoReader {
     if (!this.videoEl) {
       throw new Error('Video element is null!');
     }
-
     removeAllListeners(this.videoEl, 'playing');
     removeAllListeners(this.videoEl, 'timeupdate');
-    this.videoEl = null;
+    if (videoReaderBox && this.videoEl) {
+      videoReaderBox.removeChild(this.videoEl);
+      URL.revokeObjectURL(this.objectURL);
+      this.videoEl = null;
+    }
   }
 
   private onTimeupdate() {
