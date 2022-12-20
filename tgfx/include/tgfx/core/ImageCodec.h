d69e60aeeeca3d7534e9cb1612df48ced9b3794e
domrjchen <dom@idom.me>
2022-12-20 10:23:08 +0800

    Fix building error in ImageCodec.

--- tgfx/include/tgfx/core/ImageCodec.h
+++ tgfx/include/tgfx/core/ImageCodec.h
@@ -63,6 +63,12 @@ class ImageCodec : public ImageGenerator {
   }
 
   /**
+   * Crates a new texture buffer capturing the pixels in this image. ImageCodec do not cache the
+   * newly created texture buffer, each call to this method allocates additional storage.
+   */
+  std::shared_ptr<ImageBuffer> makeBuffer() const override;
+
+  /**
    * Decodes the image with the specified image info into the given pixels. Returns true if the
    * decoding was successful.
    * Note: This method is not implemented on the web platform, use makeBuffer() instead if your
