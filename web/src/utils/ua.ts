845616f90cf828426a127ebed1794476dee9c69b
zenos <zenoslin@outlook.com>
2023-02-10 16:56:35 +0800

    Fix get user agent fail on WeChat. (#718)

--- web/src/utils/ua.ts
+++ web/src/utils/ua.ts
@@ -1,4 +1,4 @@
-const nav = navigator.userAgent;
+const nav = navigator?.userAgent || '';
 export const ANDROID = /android|adr/i.test(nav);
 export const MOBILE = /(mobile)/i.test(nav) && ANDROID;
 export const MACOS = !(/(mobile)/i.test(nav) || MOBILE) && /Mac OS X/i.test(nav);
