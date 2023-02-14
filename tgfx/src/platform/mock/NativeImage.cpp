80a2502af992b38df9dfe347546ec875be071206
domrjchen <dom@idom.me>
2023-02-14 09:34:19 +0800

    Fix build error on linux.

--- tgfx/src/platform/mock/NativeImage.cpp
+++ tgfx/src/platform/mock/NativeImage.cpp
@@ -27,7 +27,7 @@ std::shared_ptr<ImageCodec> NativeCodec::MakeCodec(std::shared_ptr<Data>) {
   return nullptr;
 }
 
-std::shared_ptr<ImageCodec> NativeCodec::MakeFrom(NativeImageRef) {
+std::shared_ptr<ImageCodec> ImageCodec::MakeFrom(NativeImageRef) {
   return nullptr;
 }
 }  // namespace tgfx
