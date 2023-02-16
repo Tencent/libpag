008b28c6e4919c7159818e577ce0a16eae0be48e
zenoslin <zenoslin@outlook.com>
2023-02-16 17:23:36 +0800

    Implements ByteBuffer.

--- tgfx/src/platform/web/NativeImageInfo.cpp
+++ tgfx/src/platform/web/NativeImageInfo.cpp
@@ -21,125 +21,88 @@
 using namespace emscripten;
 
 namespace tgfx {
-class IIBuffer {
- public:
-  explicit IIBuffer(std::shared_ptr<Data> imageBytes) : _imageBytes(std::move(imageBytes)) {
-  }
-
-  const void* data() const {
-    return _imageBytes->data();
-  }
-
-  size_t size() const {
-    return _imageBytes->size();
-  }
-
-  bool cmp(off_t offset, size_t size, const void* buffer) {
-    return memcmp(_imageBytes->bytes() + offset, buffer, size) == 0;
-  }
-
-  bool cmpAnyOf(off_t offset, size_t size, const std::initializer_list<const void*>& bufList) {
-    return std::any_of(bufList.begin(), bufList.end(), [this, offset, size](const void* buf) {
-      return memcmp(_imageBytes->bytes() + offset, buf, size) == 0;
-    });
-  }
-
-  std::string readString(off_t offset, size_t size) {
-    return std::string((char*)_imageBytes->bytes() + offset, size);
-  }
-
-  uint8_t readU8(off_t offset) {
-    return *((uint8_t*)(_imageBytes->bytes() + offset));
-  }
-
-  uint16_t readU16LE(off_t offset) {
-    return readU8(offset) + (readU8(offset + 1) << 8);
-  }
-
-  uint16_t readU16BE(off_t offset) {
-    return (readU8(offset) << 8) + readU8(offset + 1);
-  }
-
-  uint32_t readU32LE(off_t offset) {
-    return readU8(offset) + (readU8(offset + 1) << 8) + (readU8(offset + 2) << 16) +
-           (readU8(offset + 3) << 24);
-  }
-
-  uint32_t readU32BE(off_t offset) {
-    return (readU8(offset) << 24) + (readU8(offset + 1) << 16) + (readU8(offset + 2) << 8) +
-           readU8(offset + 3);
-  }
-
- private:
-  std::shared_ptr<Data> _imageBytes = nullptr;
-};
-
-static bool IsPng(const std::shared_ptr<IIBuffer>& buffer, int& width, int& height) {
-  if (buffer->size() < 4) {
+static bool IsPng(ByteBuffer buffer, int& width, int& height) {
+  if (buffer.length() < 4) {
     return false;
   }
-  if (!buffer->cmp(0, 4, "\x89PNG")) {
+  if (buffer.readString(4) != "\x89PNG") {
+    buffer.setPosition(0);
     return false;
   }
-  std::string firstChunkType = buffer->readString(12, 4);
-  if (firstChunkType == "IHDR" && buffer->size() >= 24) {
-    width = buffer->readU32BE(16);
-    height = buffer->readU32BE(20);
+  buffer.skip(8);
+  std::string firstChunkType = buffer.readString(4);
+  if (firstChunkType == "IHDR" && buffer.length() >= 24) {
+    buffer.setOrder(ByteOrder::BigEndian);
+    width = buffer.readUint32();
+    height = buffer.readUint32();
     return true;
   } else if (firstChunkType == "CgBI") {
-    if (buffer->readString(28, 4) == "IHDR" && buffer->size() >= 40) {
-      width = buffer->readU32BE(32);
-      height = buffer->readU32BE(36);
-      return true;
+    if (buffer.length() >= 40) {
+      buffer.skip(12);
+      if (buffer.readString(4) == "IHDR") {
+        buffer.setOrder(ByteOrder::BigEndian);
+        width = buffer.readUint32();
+        height = buffer.readUint32();
+        return true;
+      }
     }
   }
   return false;
 }
 
-static bool IsJpeg(const std::shared_ptr<IIBuffer>& buffer, int& width, int& height) {
-  if (buffer->size() < 2) {
+static bool IsJpeg(ByteBuffer buffer, int& width, int& height) {
+  if (buffer.length() < 2) {
     return false;
   }
-  if (!buffer->cmp(0, 2, "\xFF\xD8")) {
+  if (buffer.readString(2) != "\xFF\xD8") {
     return false;
   }
-  off_t offset = 2;
-  while (offset + 9 <= static_cast<long>(buffer->size())) {
-    uint16_t sectionSize = buffer->readU16BE(offset + 2);
-    if (buffer->cmpAnyOf(offset, 2, {"\xFF\xC0", "\xFF\xC1", "\xFF\xC2"})) {
-      height = buffer->readU16BE(offset + 5);
-      width = buffer->readU16BE(offset + 7);
+  buffer.setOrder(ByteOrder::BigEndian);
+  while (buffer.position() + 9 <= buffer.length()) {
+    auto target = buffer.readString(2);
+    if (target == "\xFF\xC0" || target == "\xFF\xC1" || target == "\xFF\xC2") {
+      buffer.skip(3);
+      height = buffer.readUint16();
+      width = buffer.readUint16();
       return true;
     }
-    offset += sectionSize + 2;
+    uint16_t sectionSize = buffer.readUint16();
+    buffer.skip(sectionSize - 2);
   }
   return false;
 }
 
-static bool IsWebp(const std::shared_ptr<IIBuffer>& buffer, int& width, int& height) {
-  if (buffer->size() < 16) {
+static bool IsWebp(ByteBuffer buffer, int& width, int& height) {
+  if (buffer.length() < 16) {
     return false;
   }
-  if (!buffer->cmp(0, 4, "RIFF") || !buffer->cmp(8, 4, "WEBP")) {
+  auto RIFF = buffer.readString(4);
+  buffer.skip((4));
+  auto WEBP = buffer.readString(4);
+  if (RIFF != "RIFF" || WEBP != "WEBP") {
     return false;
   }
-  std::string type = buffer->readString(12, 4);
-  if (type == "VP8 " && buffer->size() >= 30) {
-    width = buffer->readU16LE(26) & 0x3FFF;
-    height = buffer->readU16LE(28) & 0x3FFF;
+  std::string type = buffer.readString(4);
+  if (type == "VP8 " && buffer.length() >= 30) {
+    buffer.skip(10);
+    width = buffer.readUint16() & 0x3FFF;
+    height = buffer.readUint16() & 0x3FFF;
     return true;
-  } else if (type == "VP8L" && buffer->size() >= 25) {
-    uint32_t n = buffer->readU32LE(21);
+  } else if (type == "VP8L" && buffer.length() >= 25) {
+    buffer.skip(5);
+    uint32_t n = buffer.readUint32();
     width = (n & 0x3FFF) + 1;
     height = ((n >> 14) & 0x3FFF) + 1;
     return true;
-  } else if (type == "VP8X" && buffer->size() >= 30) {
-    uint8_t extendedHeader = buffer->readU8(20);
+  } else if (type == "VP8X" && buffer.length() >= 30) {
+    buffer.skip(4);
+    uint8_t extendedHeader = buffer.readUint8();
     bool validStart = (extendedHeader & 0xc0) == 0;
     bool validEnd = (extendedHeader & 0x01) == 0;
     if (validStart && validEnd) {
-      width = (buffer->readU32LE(24) & 0x00FFFFFF) + 1;
-      height = (buffer->readU32LE(27) & 0x00FFFFFF) + 1;
+      buffer.skip(3);
+      width = buffer.readUint24() + 1;
+      height = buffer.readUint24() + 1;
       return true;
     }
   }
@@ -157,7 +120,7 @@ static bool CheckWebpSupport() {
 ISize NativeImageInfo::GetSize(std::shared_ptr<Data> imageBytes) {
   static const bool hasWebpSupport = CheckWebpSupport();
   auto imageSize = ISize::MakeEmpty();
-  auto buffer = std::make_shared<IIBuffer>(std::move(imageBytes));
+  ByteBuffer buffer(imageBytes->bytes(), imageBytes->size());
   if (IsPng(buffer, imageSize.width, imageSize.height)) {
     return imageSize;
   }
