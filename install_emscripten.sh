5e21e7d18d20aaa973f03600b78a3cc6dfe4a524
zenoslin <zenoslin@outlook.com>
2023-02-17 16:09:03 +0800

    Use emsdk to install emscripten.

--- install_emscripten.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(brew --repo homebrew/core)" && git checkout d69507037bf458dd4a090dee764f8106fcd8cefd
-HOMEBREW_NO_AUTO_UPDATE=1 brew install emscripten || exit 1
-git checkout master
-brew pin emscripten || exit 1
