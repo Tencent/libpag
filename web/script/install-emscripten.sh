c9cfe3b7fa3a361d16de272eee590d0979b388b3
zenoslin <zenoslin@outlook.com>
2023-02-20 10:43:31 +0800

    Link all file in emscripten.

--- web/script/install-emscripten.sh
+++ web/script/install-emscripten.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -e
+
+INSTALL_PATH="/usr/local/Cellar/emsdk"
+
 echo "-----install emscripten begin-----"
-if [ ! -d "/usr/local/Cellar/emsdk" ]; then
-  git clone https://github.com/emscripten-core/emsdk.git /usr/local/Cellar/emsdk || exit 1
+if [ ! -d $INSTALL_PATH ]; then
+ git clone https://github.com/emscripten-core/emsdk.git $INSTALL_PATH || exit 1
 fi
 
-/usr/local/Cellar/emsdk/emsdk install 3.1.20 || exit 1
-/usr/local/Cellar/emsdk/emsdk activate 3.1.20 || exit 1
+"$INSTALL_PATH/emsdk" install 3.1.20 || exit 1
+"$INSTALL_PATH/emsdk" activate 3.1.20 || exit 1
 
-LINK_PATH="em++ em-config emar embuilder emcc emcmake emconfigure emdump emdwp emmake emnm emrun emprofile emscons emsize emstrip emsymbolizer emcc.py emcmake.py emar.py"
+LINK_PATH=$(ls $INSTALL_PATH/upstream/emscripten/ | grep ^em)
 
 for i in $LINK_PATH; do
-  if [ ! -f "/usr/local/bin/$i" ]; then
-    ln -s "/usr/local/Cellar/emsdk/upstream/emscripten/$i" "/usr/local/bin/$i"
-  fi
+ if [ ! -f "/usr/local/bin/$i" ]; then
+   ln -s "$INSTALL_PATH/upstream/emscripten/$i" "/usr/local/bin/$i"
+ fi
 done
+
 echo "-----install emscripten end-----"
