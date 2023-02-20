1f6a07b11876cb56f301266b8eb2b8ad7963af6c
zenos <zenoslin@outlook.com>
2023-02-20 13:14:47 +0800

    Use emsdk to install emscripten. (#733)
    * Use emsdk to install emscripten.
    
    * Link all file in emscripten.
    
    ---------
    
    Co-authored-by: Dom Chen <dom@idom.me>

--- web/script/install-emscripten.sh
+++ web/script/install-emscripten.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -e
+
+INSTALL_PATH="/usr/local/Cellar/emsdk"
+
 echo "-----install emscripten begin-----"
-if [ ! -d "/usr/local/Cellar/emsdk" ]; then
-  git clone https://github.com/emscripten-core/emsdk.git /usr/local/Cellar/emsdk
-  /usr/local/Cellar/emsdk/emsdk install 3.1.20
-  /usr/local/Cellar/emsdk/emsdk activate 3.1.20
+if [ ! -d $INSTALL_PATH ]; then
+ git clone https://github.com/emscripten-core/emsdk.git $INSTALL_PATH || exit 1
 fi
 
-source /usr/local/Cellar/emsdk/emsdk_env.sh
+"$INSTALL_PATH/emsdk" install 3.1.20 || exit 1
+"$INSTALL_PATH/emsdk" activate 3.1.20 || exit 1
+
+LINK_PATH=$(ls $INSTALL_PATH/upstream/emscripten/ | grep ^em)
+
+for i in $LINK_PATH; do
+ if [ ! -f "/usr/local/bin/$i" ]; then
+   ln -s "$INSTALL_PATH/upstream/emscripten/$i" "/usr/local/bin/$i"
+ fi
+done
+
+echo "-----install emscripten end-----"
