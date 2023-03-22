d86116db5fbd02744b3a01513477960b573b063f
zenos <zenoslin@outlook.com>
2023-03-22 16:17:33 +0800

    Enable antialias and use linear filtering on Web Lite SDK. (#803)

--- web/lite/src/view/utils.ts
+++ web/lite/src/view/utils.ts
@@ -69,8 +69,8 @@ export const createAndSetupTexture = (gl: WebGLRenderingContext) => {
   // working with pixels.
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
   return texture;
 };
 
