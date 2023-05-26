9cba0dbd0daf7490221dbbd044fe1d2e5117d849
kevingpqi <kevingpqi@tencent.com>
2023-05-26 16:08:47 +0800

    update

--- android/libpag/src/main/java/org/extra/tools/LifecycleFragment.java
+++ android/libpag/src/main/java/org/extra/tools/LifecycleFragment.java
@@ -1,28 +1,35 @@
 package org.extra.tools;
 
 import android.app.Fragment;
-
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.Set;
+import java.util.WeakHashMap;
 
 public class LifecycleFragment extends Fragment {
-    private CopyOnWriteArraySet<LifecycleListener> lifecycleListeners = new CopyOnWriteArraySet<>();
+    private final Set<LifecycleListener> lifecycleListeners =
+            Collections.newSetFromMap(new WeakHashMap<LifecycleListener, Boolean>());
 
+    private final Object lock = new Object();
     public LifecycleFragment() {
     }
 
     public void addListener(LifecycleListener listener) {
-        lifecycleListeners.add(listener);
+        synchronized (lock) {
+            lifecycleListeners.add(listener);
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Iterator<LifecycleListener> iterator = lifecycleListeners.iterator();
-        while (iterator.hasNext()) {
-            LifecycleListener lifecycleListener = iterator.next();
-            if (lifecycleListener != null) {
-                lifecycleListener.onResume();
+        synchronized (lock) {
+            Iterator<LifecycleListener> iterator = lifecycleListeners.iterator();
+            while (iterator.hasNext()) {
+                LifecycleListener lifecycleListener = iterator.next();
+                if (lifecycleListener != null) {
+                    lifecycleListener.onResume();
+                }
             }
         }
     }
