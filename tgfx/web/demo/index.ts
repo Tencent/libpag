bdbf1f3afb5bdecea44988f85a24b67f3b576c68
zhalice2011 <zhalice2012@gmail.com>
2023-07-18 18:17:01 +0800

    Moved code related to tgfx from web folder to tgfx folder

--- tgfx/web/demo/index.ts
+++ tgfx/web/demo/index.ts
@@ -1,16 +1,12 @@
-import { TGFXInit, types } from '../src/main'
+import { TGFXInit, types } from '../src/main';
 
 let TGFXModule: types.TGFX;
 
 window.onload = async () => {
-  TGFXModule = await TGFXInit({ locateFile: (file: string) => '../lib/' + file })
-  console.log(`wasm loaded!`, TGFXModule)
+  TGFXModule = await TGFXInit({ locateFile: (file: string) => '../lib/' + file });
+  console.log(`wasm loaded!`, TGFXModule);
   document.getElementById('waiting')!.style.display = 'none';
   document.getElementById('container')!.style.display = 'block';
   TGFXModule.ScalerContext.isEmoji('测试');
   TGFXModule.ScalerContext.isEmoji('👍');
-}
-
-
-
-  
+};
