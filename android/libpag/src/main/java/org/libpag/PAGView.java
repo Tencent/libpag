86265890a754db38c938072defae5e7622ea45f7
JackFan <635555698@qq.com>
2023-09-21 10:47:21 +0800

    Fix the issue where PAG would re-display after being hidden when used in Android's RecyclerView.  (#1710)
    * fix: When calling the setVisibility function,it does not necessarily trigger a callback to the onVisibilityAggregated method.Therefore, it is necessary to handle this specific situation on your own.
    
    * feat:improve PAGView code.
    
    * feat:remove PAGView redudant code
    
    * feat:improve PAGView code style.
    
    ---------
    
    Co-authored-by: fanmingyi <fanmingyi@lizhi.fm>

--- android/libpag/src/main/java/org/libpag/PAGView.java
+++ android/libpag/src/main/java/org/libpag/PAGView.java
@@ -29,10 +29,8 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.TextureView;
 import android.view.View;
-
 import org.extra.tools.Lifecycle;
 import org.extra.tools.LifecycleListener;
-
 import java.util.ArrayList;
 
 
@@ -631,6 +629,15 @@ public class PAGView extends TextureView implements TextureView.SurfaceTextureLi
         for (PAGViewListener listener : arrayList) {
             listener.onAnimationEnd(this);
         }
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+        // When calling the setVisibility function,
+        // it does not necessarily trigger a callback to the onVisibilityAggregated method.
+        // Therefore, it is necessary to handle this specific situation on your own.
+        checkVisible();
     }
 
     public void onAnimationCancel(PAGAnimator animator) {
