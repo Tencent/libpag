77ed54057d94526dd028e5b3b637803cc581b6a5
kevingpqi <kevingpqi@tencent.com>
2024-08-01 15:20:56 +0800

    Modify to depend on the CMake file in the root directory.

--- ohos/libpag/src/main/cpp/napi_init.cpp
+++ ohos/libpag/src/main/cpp/napi_init.cpp
@@ -10,7 +10,7 @@ static napi_value Init(napi_env env, napi_value exports) {
 EXTERN_C_END
 
 static napi_module demoModule = {
-    1, 0, nullptr, Init, "libpag", ((void*)0), {0},
+    1, 0, nullptr, Init, "pag", ((void*)0), {0},
 };
 
 extern "C" __attribute__((constructor)) void RegisterEntryModule(void) {
