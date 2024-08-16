57278ee5547b20c4077069eddf0501cb85201397
Hparty <420024556@qq.com>
2024-08-16 15:12:32 +0800

    Fix crash caused by PAGView destructor (#2431)

--- src/platform/ohos/JPAGView.cpp
+++ src/platform/ohos/JPAGView.cpp
@@ -52,8 +52,13 @@ void OnSurfaceCreatedCB(OH_NativeXComponent* component, void* window) {
   }
   auto drawable = pag::GPUDrawable::FromWindow(static_cast<NativeWindow*>(window));
   auto view = ViewMap[id];
-  view->player->setSurface(pag::PAGSurface::MakeFrom(drawable));
-  view->animator->update();
+  auto player = view->getPlayer();
+  auto animator = view->getAnimator();
+  if (player == nullptr || animator == nullptr) {
+    return;
+  }
+  player->setSurface(pag::PAGSurface::MakeFrom(drawable));
+  animator->update();
 }
 
 void OnSurfaceChangedCB(OH_NativeXComponent* component, void* window) {
@@ -73,7 +78,12 @@ void OnSurfaceChangedCB(OH_NativeXComponent* component, void* window) {
     LOGE("Could not find PAGView on Surface change id:%s", id.c_str());
     return;
   }
-  auto surface = ViewMap[id]->player->getSurface();
+  auto view = ViewMap[id];
+  auto player = view->getPlayer();
+  if (player == nullptr) {
+    return;
+  }
+  auto surface = player->getSurface();
   if (!surface) {
     LOGE("PAGView id:%s without PAGSurface while surface size change", id.c_str());
     return;
@@ -98,7 +108,12 @@ void OnSurfaceDestroyedCB(OH_NativeXComponent* component, void* window) {
     LOGE("Could not find PAGView on Surface Destroyed id:%s", id.c_str());
     return;
   }
-  ViewMap[id]->player->setSurface(nullptr);
+  auto view = ViewMap[id];
+  auto player = view->getPlayer();
+  if (player == nullptr) {
+    return;
+  }
+  player->setSurface(nullptr);
 }
 
 static void DispatchTouchEventCB(OH_NativeXComponent*, void*) {
@@ -113,7 +128,10 @@ static napi_value Flush(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->animator->update();
+    auto animator = view->getAnimator();
+    if (animator != nullptr) {
+      animator->update();
+    }
   }
   return nullptr;
 }
@@ -131,7 +149,10 @@ static napi_value SetProgress(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->animator->setProgress(progress);
+    auto animator = view->getAnimator();
+    if (animator != nullptr) {
+      animator->setProgress(progress);
+    }
   }
   return nullptr;
 }
@@ -148,14 +169,19 @@ static napi_value SetComposition(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
+    auto player = view->getPlayer();
+    auto animator = view->getAnimator();
+    if (player == nullptr || animator == nullptr) {
+      return nullptr;
+    }
     if (layer != nullptr) {
       if (layer->layerType() == LayerType::PreCompose) {
-        view->player->setComposition(std::static_pointer_cast<PAGComposition>(layer));
-        view->animator->setDuration(layer->duration());
+        player->setComposition(std::static_pointer_cast<PAGComposition>(layer));
+        animator->setDuration(layer->duration());
       }
     } else {
-      view->player->setComposition(nullptr);
-      view->animator->setDuration(0);
+      player->setComposition(nullptr);
+      animator->setDuration(0);
     }
   }
   return nullptr;
@@ -174,7 +200,10 @@ static napi_value SetRepeatCount(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->animator->setRepeatCount(repeatCount);
+    auto animator = view->getAnimator();
+    if (animator) {
+      animator->setRepeatCount(repeatCount);
+    }
   }
   return nullptr;
 }
@@ -186,9 +215,15 @@ static napi_value Play(napi_env env, napi_callback_info info) {
   napi_get_cb_info(env, info, &argc, args, &jsView, nullptr);
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
+
   if (view != nullptr) {
-    view->player->prepare();
-    view->animator->start();
+    auto player = view->getPlayer();
+    auto animator = view->getAnimator();
+    if (player == nullptr || animator == nullptr) {
+      return nullptr;
+    }
+    player->prepare();
+    animator->start();
   }
   return nullptr;
 }
@@ -201,7 +236,10 @@ static napi_value Pause(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->animator->cancel();
+    auto animator = view->getAnimator();
+    if (animator) {
+      animator->cancel();
+    }
   }
   return nullptr;
 }
@@ -288,7 +326,10 @@ static napi_value SetSync(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->animator->setSync(value);
+    auto animator = view->getAnimator();
+    if (animator) {
+      animator->setSync(value);
+    }
   }
   return nullptr;
 }
@@ -306,7 +347,10 @@ static napi_value SetVideoEnabled(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->player->setVideoEnabled(value);
+    auto player = view->getPlayer();
+    if (player) {
+      player->setVideoEnabled(value);
+    }
   }
   return nullptr;
 }
@@ -324,7 +368,10 @@ static napi_value SetCacheEnabled(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->player->setCacheEnabled(value);
+    auto player = view->getPlayer();
+    if (player) {
+      player->setCacheEnabled(value);
+    }
   }
   return nullptr;
 }
@@ -342,7 +389,10 @@ static napi_value SetCacheScale(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->player->setCacheScale(value);
+    auto player = view->getPlayer();
+    if (player) {
+      player->setCacheScale(value);
+    }
   }
   return nullptr;
 }
@@ -360,7 +410,10 @@ static napi_value SetMaxFrameRate(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->player->setMaxFrameRate(value);
+    auto player = view->getPlayer();
+    if (player) {
+      player->setMaxFrameRate(value);
+    }
   }
   return nullptr;
 }
@@ -378,7 +431,10 @@ static napi_value SetScaleMode(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->player->setScaleMode(value);
+    auto player = view->getPlayer();
+    if (player) {
+      player->setScaleMode(value);
+    }
   }
   return nullptr;
 }
@@ -394,7 +450,10 @@ static napi_value SetMatrix(napi_env env, napi_callback_info info) {
   JPAGView* view = nullptr;
   napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
   if (view != nullptr) {
-    view->player->setMatrix(GetMatrix(env, args[0]));
+    auto player = view->getPlayer();
+    if (player) {
+      player->setMatrix(GetMatrix(env, args[0]));
+    }
   }
   return nullptr;
 }
@@ -409,8 +468,12 @@ static napi_value CurrentFrame(napi_env env, napi_callback_info info) {
   if (view == nullptr) {
     return nullptr;
   }
+  auto player = view->getPlayer();
+  if (!player) {
+    return nullptr;
+  }
   napi_value result;
-  napi_create_int32(env, view->player->currentFrame(), &result);
+  napi_create_int32(env, player->currentFrame(), &result);
   return result;
 }
 
@@ -424,11 +487,15 @@ static napi_value GetLayersUnderPoint(napi_env env, napi_callback_info info) {
   if (!view) {
     return nullptr;
   }
+  auto player = view->getPlayer();
+  if (!player) {
+    return nullptr;
+  }
   double surfaceX = 0;
   napi_get_value_double(env, args[0], &surfaceX);
   double surfaceY = 0;
   napi_get_value_double(env, args[1], &surfaceY);
-  auto layers = view->player->getLayersUnderPoint(surfaceX, surfaceY);
+  auto layers = player->getLayersUnderPoint(surfaceX, surfaceY);
   napi_value result;
   napi_create_array_with_length(env, layers.size(), &result);
   for (uint32_t i = 0; i < layers.size(); i++) {
@@ -451,7 +518,11 @@ static napi_value FreeCache(napi_env env, napi_callback_info info) {
   if (view == nullptr) {
     return nullptr;
   }
-  auto surface = view->player->getSurface();
+  auto player = view->getPlayer();
+  if (!player) {
+    return nullptr;
+  }
+  auto surface = player->getSurface();
   if (surface) {
     surface->freeCache();
   }
@@ -468,8 +539,26 @@ static napi_value MakeSnapshot(napi_env env, napi_callback_info info) {
   if (view == nullptr) {
     return nullptr;
   }
-  auto surface = view->player->getSurface();
+  auto player = view->getPlayer();
+  if (!player) {
+    return nullptr;
+  }
+  auto surface = player->getSurface();
   return MakeSnapshot(env, surface.get());
+}
+
+static napi_value Release(napi_env env, napi_callback_info info) {
+  napi_value jsView = nullptr;
+  size_t argc = 0;
+  napi_value args[1] = {0};
+  napi_get_cb_info(env, info, &argc, args, &jsView, nullptr);
+  JPAGView* view = nullptr;
+  napi_unwrap(env, jsView, reinterpret_cast<void**>(&view));
+  if (view == nullptr) {
+    return nullptr;
+  }
+  view->release();
+  return nullptr;
 }
 
 napi_value JPAGView::Constructor(napi_env env, napi_callback_info info) {
@@ -480,13 +569,11 @@ napi_value JPAGView::Constructor(napi_env env, napi_callback_info info) {
   std::string id = "PAGView" + std::to_string(UniqueID::Next());
   auto cView = std::make_shared<JPAGView>(id);
   cView->animator = PAGAnimator::MakeFrom(cView);
-  cView->animator->setRepeatCount(-1);
   napi_wrap(
       env, jsView, cView.get(),
       [](napi_env, void* finalize_data, void*) {
         JPAGView* view = static_cast<JPAGView*>(finalize_data);
         ViewMap.erase(view->id);
-        delete view;
       },
       nullptr, nullptr);
   ViewMap.emplace(id, cView);
@@ -514,7 +601,8 @@ bool JPAGView::Init(napi_env env, napi_value exports) {
       PAG_DEFAULT_METHOD_ENTRY(currentFrame, CurrentFrame),
       PAG_DEFAULT_METHOD_ENTRY(getLayersUnderPoint, GetLayersUnderPoint),
       PAG_DEFAULT_METHOD_ENTRY(freeCache, FreeCache),
-      PAG_DEFAULT_METHOD_ENTRY(makeSnapshot, MakeSnapshot)};
+      PAG_DEFAULT_METHOD_ENTRY(makeSnapshot, MakeSnapshot),
+      PAG_DEFAULT_METHOD_ENTRY(release, Release)};
   auto status = DefineClass(env, exports, ClassName(), sizeof(classProp) / sizeof(classProp[0]),
                             classProp, Constructor, "");
   if (status != napi_ok) {
@@ -541,32 +629,83 @@ bool JPAGView::Init(napi_env env, napi_value exports) {
   return true;
 }
 
-void JPAGView::onAnimationStart(PAGAnimator*) {
+JPAGView::~JPAGView() {
+  release();
+}
 
-  napi_call_threadsafe_function(playingStateCallback, &PAGViewStateStart,
-                                napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+void JPAGView::onAnimationStart(PAGAnimator*) {
+  std::lock_guard lock_guard(locker);
+  if (playingStateCallback) {
+    napi_call_threadsafe_function(playingStateCallback, &PAGViewStateStart,
+                                  napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  }
 }
 
 void JPAGView::onAnimationCancel(PAGAnimator*) {
-  napi_call_threadsafe_function(playingStateCallback, &PAGViewStateCancel,
-                                napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  std::lock_guard lock_guard(locker);
+  if (playingStateCallback) {
+    napi_call_threadsafe_function(playingStateCallback, &PAGViewStateCancel,
+                                  napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  }
 }
 
 void JPAGView::onAnimationEnd(PAGAnimator*) {
-  napi_call_threadsafe_function(playingStateCallback, &PAGViewStateEnd,
-                                napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  std::lock_guard lock_guard(locker);
+  if (playingStateCallback) {
+    napi_call_threadsafe_function(playingStateCallback, &PAGViewStateEnd,
+                                  napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  }
 }
 
 void JPAGView::onAnimationRepeat(PAGAnimator*) {
-  napi_call_threadsafe_function(playingStateCallback, &PAGViewStateRepeat,
-                                napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  std::lock_guard lock_guard(locker);
+  if (playingStateCallback) {
+    napi_call_threadsafe_function(playingStateCallback, &PAGViewStateRepeat,
+                                  napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  }
 }
 
 void JPAGView::onAnimationUpdate(PAGAnimator* animator) {
-  napi_call_threadsafe_function(progressCallback, new double(animator->progress()),
-                                napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
-  player->setProgress(animator->progress());
-  player->flush();
+  std::lock_guard lock_guard(locker);
+  if (progressCallback) {
+    napi_call_threadsafe_function(progressCallback, new double(animator->progress()),
+                                  napi_threadsafe_function_call_mode::napi_tsfn_nonblocking);
+  }
+
+  if (player) {
+    player->setProgress(animator->progress());
+    player->flush();
+  }
 }
 
+void JPAGView::release() {
+  std::lock_guard lock_guard(locker);
+  if (progressCallback != nullptr) {
+    napi_release_threadsafe_function(progressCallback, napi_tsfn_abort);
+    progressCallback = nullptr;
+  }
+  if (playingStateCallback != nullptr) {
+    napi_release_threadsafe_function(playingStateCallback, napi_tsfn_abort);
+    playingStateCallback = nullptr;
+  }
+
+  if (animator) {
+    animator = nullptr;
+  }
+
+  if (player) {
+    player->setSurface(nullptr);
+    player = nullptr;
+  }
+}
+
+std::shared_ptr<PAGAnimator> JPAGView::getAnimator() {
+  std::lock_guard lock_guard(locker);
+  return animator;
+}
+
+std::shared_ptr<PAGPlayer> JPAGView::getPlayer() {
+  std::lock_guard lock_guard(locker);
+  return player;
+}
 }  // namespace pag
