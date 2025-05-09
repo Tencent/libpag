591773fdc2a4c45a36f65d32f8238c57004fcf51
billyjin <95683733+jinwuwu001@users.noreply.github.com>
2025-05-09 20:19:25 +0800

    Fix the issue of infinite canvas size growth in WeChat mini programs (#2812)
    Co-authored-by: billyjin <billyjin@tencent.com>

--- web/lite/script/rollup.config.js
+++ web/lite/script/rollup.config.js
@@ -4,27 +4,12 @@ import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import esbuild from 'rollup-plugin-esbuild';
-
 import pkg from '../package.json';
+import { readFileSync } from 'node:fs';
+import path from 'path';
 
-const banner = `/////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//  Tencent is pleased to support the open source community by making libpag available.
-//
-//  Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
-//
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-//  except in compliance with the License. You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-//  unless required by applicable law or agreed to in writing, software distributed under the
-//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
-//  either express or implied. see the license for the specific language governing permissions
-//  and limitations under the license.
-//
-/////////////////////////////////////////////////////////////////////////////////////////////////
-`;
+const fileHeaderPath = path.resolve(__dirname, '../../../.idea/fileTemplates/includes/PAG File Header.h');
+const banner = readFileSync(fileHeaderPath, 'utf-8');
 
 const name = 'libpag';
 const plugins = (needBabel = false, needTerser = false) => {
