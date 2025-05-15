b63e7711729a095e598f6f13c6ad7d12429a7562
markffan <markffan@tencent.com>
2025-05-15 15:30:42 +0800

    Implement file structure display in Viewer - Part1 (#2817)

---
+++ viewer/sync_deps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd $(dirname $0)
+
+./install_tools.sh
+
+if [[ `uname` == 'Darwin' ]]; then
+  if [ ! $(which emcc) ]; then
+      echo "emscripten not found. Trying to install..."
+      brew install emscripten
+  fi
+fi
+
+if [ ! $(which depsync) ]; then
+  echo "depsync not found. Trying to install..."
+  npm install -g depsync > /dev/null
+else
+  npm update -g depsync > /dev/null
+fi
+
+depsync || exit 1
