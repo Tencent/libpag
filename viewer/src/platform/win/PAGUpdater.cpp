597ad2f5ef9f2b3a60cb1339b8908e33ae65b70d
markffan <markffan@tencent.com>
2025-06-04 10:00:49 +0800

    Add updater for viewer on windows

---
+++ viewer/src/platform/win/PAGUpdater.cpp
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Tencent is pleased to support the open source community by making libpag available.
+//
+//  Copyright (C) 2025 THL A29 Limited, a Tencent company. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "PAGUpdater.h"
+#include "version.h"
+#include "rendering/PAGWindow.h"
+#include <codecvt>
+#include <winsparkle/winsparkle.h>
+
+namespace pag {
+
+static void win_sparkle_did_find_update_callback() {
+  for (int i = 0; i < PAGWindow::AllWindows.size(); i++) {
+    auto window = PAGWindow::AllWindows[i];
+    auto root = window->getEngine()->rootObjects().first();
+    if (root) {
+      QMetaObject::invokeMethod(root, "updateAvailable", Q_ARG(QVariant, true));
+    }
+  }
+}
+
+void PAGUpdater::initUpdater() {
+  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+  auto wVersion = converter.from_bytes(AppVersion);
+  win_sparkle_set_appcast_url("");
+  win_sparkle_set_app_details(L"pag.art", L"PAGViewer", wVersion.c_str());
+  win_sparkle_set_automatic_check_for_updates(0);
+  win_sparkle_init();
+}
+
+void PAGUpdater::checkForUpdates(bool keepSilent, const std::string& url) {
+  win_sparkle_set_appcast_url(url.c_str());
+  win_sparkle_set_did_find_update_callback(win_sparkle_did_find_update_callback);
+  if (keepSilent) {
+    win_sparkle_check_update_without_ui();
+  } else {
+    win_sparkle_check_update_with_ui();
+  }
+}
+
+}
