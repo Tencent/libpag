78878fdae2e6464b91de37fa009b507b49de821c
markffan <markffan@tencent.com>
2026-01-09 19:31:38 +0800

    Fix PAGExporter to load Qt resources from external shared directory. (#3196)

--- viewer/qttools/delete_qt_resource.sh
+++ viewer/qttools/delete_qt_resource.sh
@@ -1,64 +1,73 @@
+#!/bin/bash
+
+# Shared directory for PAGExporter Qt resources
+SHARED_QT_DIR="/Library/Application Support/PAGExporter"
+
+# Legacy paths for cleaning up old Qt resources copied into AE apps
 AE_PLUGIN_PATH='/Library/Application Support/Adobe/Common/Plug-ins/7.0/MediaCore'
 AE_EXPORTER_PATH="$AE_PLUGIN_PATH/PAGExporter.plugin"
-QT_FRAMEWORK_DIR="$AE_EXPORTER_PATH/Contents/Frameworks"
-QT_PLUGINS_DIR="$AE_EXPORTER_PATH/Contents/PlugIns"
-QT_RESOURCE_DIR="$AE_EXPORTER_PATH/Contents/Resources"
 
-function doDeleteFileInApps() {
-    aeAppPath="$1"
-    if [ -d "$aeAppPath" ]; then
-      deletePath="$aeAppPath/Contents/$parentDir/$deleteFile"
-      if [ -d "$deletePath" ]; then
-        rm -fr "$deletePath"
-      elif [ -f "$deletePath" ]; then
-        rm -f "$deletePath"
-      fi
-    fi
-}
+echo "Cleaning up PAGExporter Qt resources..."
 
-function deleteFileInAEApps() {
-  deleteFile="$1"
-  parentDir="$2"
-  for((i=2017;i<=2030;i++));
-  do
-    aeAppPath="/Applications/Adobe After Effects $i/Adobe After Effects $i.app"
-    doDeleteFileInApps "$aeAppPath"
+# Remove the shared Qt directory (current version)
+if [ -d "$SHARED_QT_DIR" ]; then
+    echo "  Removing shared Qt directory: $SHARED_QT_DIR"
+    sudo rm -rf "$SHARED_QT_DIR"
+    echo "  Shared Qt directory removed"
+fi
 
-    aeAppPath2="/Applications/Adobe After Effects CC $i/Adobe After Effects CC $i.app"
-    doDeleteFileInApps "$aeAppPath2"
-  done
-}
-
-function deleteFileInMEApps() {
-  deleteFile="$1"
-  parentDir="$2"
-  for((i=2017;i<=2030;i++));
-  do
-    aeAppPath="/Applications/Adobe Media Encoder $i/Adobe Media Encoder $i.app"
-    doDeleteFileInApps "$aeAppPath"
-
-    aeAppPath2="/Applications/Adobe Media Encoder CC $i/Adobe Media Encoder CC $i.app"
-    doDeleteFileInApps "$aeAppPath2"
-  done
-}
-
-# Traverse directory and delete corresponding files in AE apps
-function traversalAndDeleteFiles() {
-    cd "$1"
-    for file in *
-    do
-
-      deleteFileInAEApps "$file" "$2"
+# Clean up legacy Qt resources from AE apps (for users upgrading from old versions)
+function deleteFileFromAEApps() {
+    local deleteFile="$1"
+    local parentDir="$2"
+    for ((i=2017; i<=2030; i++)); do
+        for appPath in "/Applications/Adobe After Effects $i/Adobe After Effects $i.app" \
+                       "/Applications/Adobe After Effects CC $i/Adobe After Effects CC $i.app"; do
+            if [ -d "$appPath" ]; then
+                local targetPath="$appPath/Contents/$parentDir/$deleteFile"
+                if [ -d "$targetPath" ]; then
+                    rm -rf "$targetPath"
+                elif [ -f "$targetPath" ]; then
+                    rm -f "$targetPath"
+                fi
+            fi
+        done
     done
 }
 
-function deleteOldQtResources() {
-  if [ ! -d "$AE_EXPORTER_PATH" ]; then
-      return
-  fi
-  traversalAndDeleteFiles "$QT_FRAMEWORK_DIR" "Frameworks"
-  traversalAndDeleteFiles "$QT_PLUGINS_DIR" "PlugIns"
-  traversalAndDeleteFiles "$QT_RESOURCE_DIR" "Resources"
+function cleanupLegacyQtResources() {
+    if [ ! -d "$AE_EXPORTER_PATH" ]; then
+        return
+    fi
+
+    # Clean up Qt frameworks copied to AE apps
+    if [ -d "$AE_EXPORTER_PATH/Contents/Frameworks" ]; then
+        for item in "$AE_EXPORTER_PATH/Contents/Frameworks"/*; do
+            if [ -e "$item" ]; then
+                deleteFileFromAEApps "$(basename "$item")" "Frameworks"
+            fi
+        done
+    fi
+
+    # Clean up Qt plugins copied to AE apps
+    if [ -d "$AE_EXPORTER_PATH/Contents/PlugIns" ]; then
+        for item in "$AE_EXPORTER_PATH/Contents/PlugIns"/*; do
+            if [ -e "$item" ]; then
+                deleteFileFromAEApps "$(basename "$item")" "PlugIns"
+            fi
+        done
+    fi
+
+    # Clean up Qt resources copied to AE apps
+    if [ -d "$AE_EXPORTER_PATH/Contents/Resources" ]; then
+        for item in "$AE_EXPORTER_PATH/Contents/Resources"/*; do
+            if [ -e "$item" ]; then
+                deleteFileFromAEApps "$(basename "$item")" "Resources"
+            fi
+        done
+    fi
 }
 
-deleteOldQtResources
+cleanupLegacyQtResources
+
+echo "PAGExporter Qt resources cleanup completed"
