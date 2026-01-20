16bc95b35f8fb8a1f11c90a098f432e94e2ee23b
Dom <dom@idom.me>
2026-01-20 17:40:39 +0800

    Fix copyright header format to match libpag style.

--- pagx/src/layers/PAGXAttributes.cpp
+++ pagx/src/layers/PAGXAttributes.cpp
@@ -4,8 +4,8 @@
 //
 //  Copyright (C) 2026 Tencent. All rights reserved.
 //
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-//  in compliance with the License. You may obtain a copy of the License at
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
