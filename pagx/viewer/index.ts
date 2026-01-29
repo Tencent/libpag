cb382c469ceee6a146d4d48681f9b3fdcdbcfa3e
Dom <dom@idom.me>
2026-01-29 11:06:06 +0800

    Rename URL parameter from src to file.

--- pagx/viewer/index.ts
+++ pagx/viewer/index.ts
@@ -590,7 +590,7 @@ async function loadPAGXFromURL(url: string) {
 
 function getPAGXUrlFromParams(): string | null {
     const params = new URLSearchParams(window.location.search);
-    return params.get('src');
+    return params.get('file');
 }
 
 function setupDragAndDrop() {
