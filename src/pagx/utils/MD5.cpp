c5b34969646f8bf54cac2d8766bb59b63d54a51e
jinwuwu001 <j15769195676@163.com>
2026-04-15 15:51:12 +0800

    Rename k, r, rotate_left in MD5.cpp to K, R, RotateLeft per project naming convention.

--- src/pagx/utils/MD5.cpp
+++ src/pagx/utils/MD5.cpp
@@ -33,11 +33,11 @@ inline uint32_t H(uint32_t x, uint32_t y, uint32_t z) {
 inline uint32_t I(uint32_t x, uint32_t y, uint32_t z) {
   return y ^ (x | ~z);
 }
-inline uint32_t rotate_left(uint32_t x, uint32_t n) {
+inline uint32_t RotateLeft(uint32_t x, uint32_t n) {
   return (x << n) | (x >> (32 - n));
 }
 
-const uint32_t k[64] = {
+const uint32_t K[64] = {
     0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
     0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
     0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
@@ -46,7 +46,7 @@ const uint32_t k[64] = {
     0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
     0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
     0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
-const uint32_t r[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+const uint32_t R[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
                       5, 9,  14, 20, 5, 9,  14, 20, 5, 9,  14, 20, 5, 9,  14, 20,
                       4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
                       6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
@@ -132,7 +132,7 @@ class MD5Impl {
       uint32_t temp = d;
       d = c;
       c = b;
-      b = b + rotate_left(a + f + k[i] + m[g], r[i]);
+      b = b + RotateLeft(a + f + K[i] + m[g], R[i]);
       a = temp;
     }
     state[0] += a;
