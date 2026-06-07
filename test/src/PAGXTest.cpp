3b4b399a9a51e20e0126910202703f7daf1bf0aa
zfw123456 <1559632263@qq.com>
2026-06-07 16:41:24 +0800

    Use system Helvetica font in NoiseFilterAllElements test for consistent tgfx and SVG rendering

--- test/src/PAGXTest.cpp
+++ test/src/PAGXTest.cpp
@@ -7599,8 +7599,7 @@ PAGX_TEST(PAGXTest, NoiseFilterAllElements) {
   pagx::FontConfig fontConfig;
   fontConfig.addFallbackTypefaces(GetFallbackTypefaces());
 
-  auto typeface =
-      Typeface::MakeFromPath(ProjectPath::Absolute("resources/font/NotoSansSC-Regular.otf"));
+  auto typeface = Typeface::MakeFromPath("/System/Library/Fonts/Helvetica.ttc");
   auto fontFamily = typeface ? typeface->fontFamily() : std::string();
   auto fontStyle = typeface ? typeface->fontStyle() : std::string();
   if (typeface) {
