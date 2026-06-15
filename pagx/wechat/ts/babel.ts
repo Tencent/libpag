056cb55d4dd9746c9f2177cb216cebf80eba4dfc
jinwuwu001 <j15769195676@163.com>
2026-06-15 16:01:55 +0800

    Remove unused wx import and declaration from babel polyfill module.

--- pagx/wechat/ts/babel.ts
+++ pagx/wechat/ts/babel.ts
@@ -19,10 +19,8 @@
 /* global globalThis */
 // Polyfills for WeChat Mini Program environment: registers WXWebAssembly as WebAssembly
 // and ensures globalThis/window are available for Emscripten module initialization.
-import type { wx } from './interfaces';
 
 declare const WXWebAssembly: typeof WebAssembly;
-declare const wx: wx;
 declare const globalThis: any;
 
 globalThis.WebAssembly = WXWebAssembly;
